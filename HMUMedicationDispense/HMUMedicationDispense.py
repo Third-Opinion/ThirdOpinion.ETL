3b5a6b22d6e13900a6965a6265e9c9f0eec284f7
Ken Sheppard <ksheppard@practicefusion.com>
2025-09-07 15:47:40 -0700

    Synchronizing job HMUMedicationDispense from AWS Glue at 2025/09/07 22:47:39

---
+++ HMUMedicationDispense/HMUMedicationDispense.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglue import DynamicFrame
+from pyspark.sql import functions as F
+from pyspark.sql.types import StringType, TimestampType, BooleanType
+import json
+import logging
+
+# Set up logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Initialize Glue context
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Configuration
+DATABASE_NAME = "hmu-healthlake-database"
+TABLE_NAME = "medication"
+REDSHIFT_CONNECTION = "Redshift connection"
+S3_TEMP_DIR = "s3://aws-glue-assets-442042533707-us-east-2/temporary/"
+
+
+
+if __name__ == "__main__":
+    main()
+    job.commit()
