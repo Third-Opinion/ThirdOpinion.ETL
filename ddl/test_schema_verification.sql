8718b56b5aaddb9cf274394021fe461f077abb76
Ken Sheppard <ken@thirdopinion.io>
2025-10-08 16:43:41 -0700

    Test

--- ddl/test_schema_verification.sql
+++ ddl/test_schema_verification.sql
@@ -1,9 +1,9 @@
 CREATE TABLE public.test_schema_verification (
-    id INTEGER NOT NULL,
+    id INTEGER NOT NULL distkey,
     test_name VARCHAR(255) NOT NULL,
     test_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     test_status VARCHAR(50)
 )
-DISTSTYLE ALL
+DISTSTYLE KEY
 SORTKEY(test_date);
     -- Added new column for testing
